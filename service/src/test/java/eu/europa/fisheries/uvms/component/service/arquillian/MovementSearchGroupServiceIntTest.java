ddbf82c0dbaae6af0dd0d4e6e1668dc9b4b6cc1b
thomasfanto <thomas.fanto@consid.se>
2017-02-27 14:45:59 +0100

    for now . . .

--- service/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/MovementSearchGroupServiceIntTest.java
+++ service/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/MovementSearchGroupServiceIntTest.java
@@ -1,6 +1,9 @@
 package eu.europa.fisheries.uvms.component.service.arquillian;
 
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -8,5 +11,31 @@ import org.junit.runner.RunWith;
  */
 
 @RunWith(Arquillian.class)
-public class MovementSearchGroupServiceIntTest {
+public class MovementSearchGroupServiceIntTest extends TransactionalTests{
+
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+        return BuildMovementTestDeployment.createDeployment();
+    }
+
+
+
+    @Test
+    public void tst(){
+
+    }
+
+
+
+
+
+
+
+    /******************************************************************************************************************
+     *   HELPER FUNCTIONS
+     ******************************************************************************************************************/
+
+
+
 }
