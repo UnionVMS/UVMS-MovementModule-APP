174646472cea26d31fac90ada890bb489ef37584
thomasfanto <thomas.fanto@consid.se>
2017-02-27 16:21:40 +0100

    added some, as it looks mandatry entries in deploymnet creator (still closer but no cigar)

--- service/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/MovementSearchGroupServiceIntTest.java
+++ service/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/MovementSearchGroupServiceIntTest.java
@@ -1,5 +1,8 @@
 package eu.europa.fisheries.uvms.component.service.arquillian;
 
+import eu.europa.ec.fisheries.uvms.movement.model.exception.MovementDuplicateException;
+import eu.europa.ec.fisheries.uvms.movement.service.MovementSearchGroupService;
+import eu.europa.ec.fisheries.uvms.movement.service.exception.MovementServiceException;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
@@ -7,12 +10,19 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.ejb.EJB;
+
 /**
  * Created by thofan on 2017-02-27.
  */
 
 @RunWith(Arquillian.class)
 public class MovementSearchGroupServiceIntTest extends TransactionalTests{
+
+
+    @EJB
+    MovementSearchGroupService movementSearchGroupService;
+
 
 
     @Deployment
@@ -23,7 +33,16 @@ public class MovementSearchGroupServiceIntTest extends TransactionalTests{
 
 
     @Test
-    public void tst(){
+    public void createMovementSearchGroup(){
+
+
+        try {
+            movementSearchGroupService.createMovementSearchGroup(null,null);
+        } catch (MovementServiceException e) {
+            e.printStackTrace();
+        } catch (MovementDuplicateException e) {
+            e.printStackTrace();
+        }
 
         Assert.assertTrue(true);
 
