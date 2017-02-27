3b107638dbc6d2e090d2c9e44dbbe9b298d5cca8
thomasfanto <thomas.fanto@consid.se>
2017-02-27 14:18:27 +0100

    added some start boilerplating for tests

---
+++ service/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/MovementSearchGroupServiceIntTest.java
@@ -0,0 +1,12 @@
+package eu.europa.fisheries.uvms.component.service.arquillian;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by thofan on 2017-02-27.
+ */
+
+@RunWith(Arquillian.class)
+public class MovementSearchGroupServiceIntTest {
+}
