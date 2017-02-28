048c752eb9b629b3356b2059213c7781e4041f60
thomasfanto <thomas.fanto@consid.se>
2017-02-28 09:02:39 +0100

    organizing deps

--- service/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/BuildMovementTestDeployment.java
+++ service/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/BuildMovementTestDeployment.java
@@ -22,40 +22,47 @@ public abstract class BuildMovementTestDeployment {
         // Embedding war package which contains the test class is needed
         // So that Arquillian can invoke test class through its servlet test runner
         WebArchive testWar = ShrinkWrap.create(WebArchive.class, "test.war");
+        
+
         //testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.constant");
         //testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.entity");
         //testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.dao");
         //testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.mapper");
+
+
         testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.service.exception");
         testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.model.exception");
         testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.message.exception");
         testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.service");
+        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.message.consumer");
+        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.message.producer");
+        testWar.addPackages(true,  "eu.europa.ec.fisheries.uvms.movement.message.event.carrier");
+        testWar.addPackages(true,  "eu.europa.ec.fisheries.uvms.movement.message.constants");
+        testWar.addPackages(true,  "eu.europa.ec.fisheries.uvms.movement.service.bean");
+        testWar.addPackages(true,  "eu.europa.ec.fisheries.uvms.movement.model");
         testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.config.constants");
         testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.config.exception");
         testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.config.module.exception");
         testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.config.model.exception");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.message.consumer");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.message.producer");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.audit.model.exception");
-        testWar.addPackages(true,  "eu.europa.ec.fisheries.uvms.movement.message.event.carrier");
         testWar.addPackages(true,  "eu.europa.ec.fisheries.uvms.config.message");
         testWar.addPackages(true,  "eu.europa.ec.fisheries.uvms.config.service");
+        testWar.addPackages(true, "eu.europa.ec.fisheries.schema");
         testWar.addPackages(true,  "eu.europa.ec.fisheries.schema.config.types.v1");
+        testWar.addPackages(true,  "eu.europa.ec.fisheries.schema.movement.search.v1");
         testWar.addPackages(true,  "eu.europa.ec.fisheries.schema.config.module.v1");
-        testWar.addPackages(true,  "eu.europa.ec.fisheries.uvms.movement.message.constants");
+        testWar.addPackages(true,  "eu.europa.ec.fisheries.uvms.user.model.exception");
+        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.audit.model.exception");
+        testWar.addPackages(true,  " eu.europa.ec.fisheries.uvms.spatial.model.exception");
+        testWar.addPackages(true,  "org.jvnet.jaxb2_commons.lang");  // << URRRK
 
-        
-        //testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.model.exception");
-        //testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.util");
+
+        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.util");
         // No no, starts threaded job...
         // Need to exclude job first...
-        //testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement");
         //testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.bean");
- //       testWar.addClass(MovementBatchModelBean.class).addClass(MovementDomainModelBean.class)
-   //             .addClass(MovementSearchGroupDomainModelBean.class).addClass(TempMovementDomainModelBean.class);
+  //      testWar.addClass(MovementBatchModelBean.class).addClass(MovementDomainModelBean.class)
+  //              .addClass(MovementSearchGroupDomainModelBean.class).addClass(TempMovementDomainModelBean.class);
 
-        //testWar.addPackages(true,"eu.europa.ec.fisheries.uvms.movement.model");
-        //testWar.addPackages(true, "eu.europa.ec.fisheries.schema");
         testWar.addClass(TransactionalTests.class);
 
 
