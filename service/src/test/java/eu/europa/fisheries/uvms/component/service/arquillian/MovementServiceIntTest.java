1380832dfe1cefbea38dd694bba471649e72db00
thomasfanto <thomas.fanto@consid.se>
2017-03-02 11:00:55 +0100

    starting

--- service/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/MovementServiceIntTest.java
+++ service/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/MovementServiceIntTest.java
@@ -1,7 +1,41 @@
 package eu.europa.fisheries.uvms.component.service.arquillian;
 
+import eu.europa.ec.fisheries.uvms.movement.service.MovementSearchGroupService;
+import eu.europa.ec.fisheries.uvms.movement.service.MovementService;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ejb.EJB;
+
 /**
  * Created by thofan on 2017-03-02.
  */
-public class MovementServiceIntTest {
+
+@RunWith(Arquillian.class)
+public class MovementServiceIntTest   extends TransactionalTests {
+
+
+    private final static String TEST_USER_NAME = "MovementServiceIntTestTestUser";
+
+    @EJB
+    MovementService movementService;
+
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+        return BuildMovementServiceTestDeployment.createDeployment();
+    }
+
+    @Test
+    public void xxx(){
+
+    }
+
+
+
+
+
 }
