2312efe596414158e0a36f10f3ead4b5871069a3
Andreas Wallen <andreasw@Andreass-MacBook-Pro.local>
2017-03-07 13:32:40 +0100

    Fixed arquillian deploy structure

--- service/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/BuildMovementServiceTestDeployment.java
+++ service/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/BuildMovementServiceTestDeployment.java
@@ -1,9 +1,5 @@
 package eu.europa.fisheries.uvms.component.service.arquillian;
 
-import eu.europa.ec.fisheries.uvms.movement.bean.MovementBatchModelBean;
-import eu.europa.ec.fisheries.uvms.movement.bean.MovementDomainModelBean;
-import eu.europa.ec.fisheries.uvms.movement.bean.MovementSearchGroupDomainModelBean;
-import eu.europa.ec.fisheries.uvms.movement.bean.TempMovementDomainModelBean;
 import eu.europa.ec.fisheries.uvms.movement.service.MovementSearchGroupService;
 import eu.europa.ec.fisheries.uvms.movement.service.MovementService;
 import eu.europa.ec.fisheries.uvms.movement.service.SpatialService;
@@ -11,9 +7,8 @@ import eu.europa.ec.fisheries.uvms.movement.service.TempMovementService;
 import eu.europa.ec.fisheries.uvms.movement.service.bean.*;
 import eu.europa.ec.fisheries.uvms.movement.service.dto.MovementDto;
 import eu.europa.ec.fisheries.uvms.movement.service.dto.MovementListResponseDto;
-import eu.europa.ec.fisheries.uvms.spatial.model.exception.SpatialException;
-import eu.europa.ec.fisheries.uvms.spatial.model.exception.SpatialModelException;
-import eu.europa.ec.fisheries.uvms.spatial.model.exception.SpatialModelMapperException;
+import eu.europa.ec.fisheries.uvms.movement.service.exception.MovementServiceException;
+import eu.europa.ec.fisheries.uvms.movement.service.validation.MovementGroupValidator;
 import eu.europa.fisheries.uvms.component.service.SpatialServiceMockedBean;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -24,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.nio.file.Files;
 import java.util.*;
 
 /**
@@ -37,53 +31,18 @@ public abstract class BuildMovementServiceTestDeployment {
 
     private static WebArchive createBasicDeployment() {
 
-        // Import Maven runtime dependencies
         File[] files = Maven.resolver().loadPomFromFile("pom.xml")
-                .importRuntimeDependencies().resolve().withTransitivity().asFile();
+                .importRuntimeAndTestDependencies().resolve().withTransitivity().asFile();
 
-
-
+        printFiles(files);
 
         // Embedding war package which contains the test class is needed
         // So that Arquillian can invoke test class through its servlet test runner
         WebArchive testWar = ShrinkWrap.create(WebArchive.class, "test.war");
 
-        // BEGIN DB and entities
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.constant");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.entity");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.dao");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.mapper");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.exception");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.model");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.service.validation");
-        // No no, starts threaded job...
-        // Need to exclude job first...
-        testWar.addClass(MovementBatchModelBean.class).addClass(MovementDomainModelBean.class)
-                .addClass(MovementSearchGroupDomainModelBean.class).addClass(TempMovementDomainModelBean.class);
-
-
-        // END
-
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.service.exception");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.model.exception");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.message.exception");
-        testWar.addPackages(true,  "eu.europa.ec.fisheries.uvms.exchange.model");
-        testWar.addPackages(true,  "eu.europa.ec.fisheries.uvms.movement.model");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.config.constants");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.config.exception");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.schema");
-        testWar.addPackages(true,  "eu.europa.ec.fisheries.schema.config.types.v1");
-        testWar.addPackages(true,  "eu.europa.ec.fisheries.schema.movement.search.v1");
-        testWar.addPackages(true,  "eu.europa.ec.fisheries.schema.config.module.v1");
-        testWar.addPackages(true,  "eu.europa.ec.fisheries.uvms.user.model.exception");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.audit.model.exception");
-        testWar.addPackages(true,  "org.jvnet.jaxb2_commons");  // << URRRK
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.movement.util");
-
         testWar.addClass(MovementConfigHelper.class);
         testWar.addClass(TransactionalTests.class);
 
-        testWar.addAsResource("persistence-integration.xml", "META-INF/persistence.xml");
         // Empty beans for EE6 CDI
         testWar.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
         testWar.addAsLibraries(files);
@@ -96,6 +55,9 @@ public abstract class BuildMovementServiceTestDeployment {
 
         archive.addClass(MovementSearchGroupServiceBean.class).addClass(MovementSearchGroupService.class);
         archive.addClass(MovementConfigHelper.class);
+        archive.addClass(MovementServiceException.class);
+        archive.addClass(MovementGroupValidator.class);
+
 
         return archive;
     }
@@ -104,6 +66,7 @@ public abstract class BuildMovementServiceTestDeployment {
     public static Archive<?> createTempMovementDeployment() {
         WebArchive archive = createBasicDeployment();
         archive.addClass(TempMovementServiceBean.class).addClass(TempMovementService.class);
+        archive.addClass(MovementServiceException.class);
 
         return archive;
     }
