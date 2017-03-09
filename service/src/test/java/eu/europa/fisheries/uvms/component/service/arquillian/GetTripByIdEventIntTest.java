d0492def4f14f96f76220406529132c632bf2390
thomasfanto <thomas.fanto@consid.se>
2017-03-09 10:28:12 +0100

    GetTripByIdEvent added

---
+++ service/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/GetTripByIdEventIntTest.java
@@ -0,0 +1,43 @@
+package eu.europa.fisheries.uvms.component.service.arquillian;
+
+import eu.europa.ec.fisheries.uvms.movement.message.event.GetTripByIdEvent;
+import eu.europa.ec.fisheries.uvms.movement.message.event.carrier.EventMessage;
+import eu.europa.ec.fisheries.uvms.movement.message.producer.bean.MessageProducerBean;
+import eu.europa.ec.fisheries.uvms.movement.model.exception.ModelMarshallException;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
+import javax.jms.JMSException;
+
+/**
+ * Created by thofan on 2017-03-09.
+ */
+
+@RunWith(Arquillian.class)
+public class GetTripByIdEventIntTest extends TransactionalTests{
+
+
+
+    @Inject
+    @GetTripByIdEvent
+    Event<EventMessage> getTripByIdEvent;
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+        return BuildMovementServiceTestDeployment.createEventDeployment();
+    }
+
+    @Test
+    public void triggerEvent() throws JMSException, ModelMarshallException {
+        System.setProperty(MessageProducerBean.MESSAGE_PRODUCER_METHODS_FAIL, "false");
+
+    }
+
+
+
+}
