42c07a03280161d1451c34c1cc4d67d1bb20e056
thomasfanto <thomas.fanto@consid.se>
2017-03-16 09:26:34 +0100

    first iteration for  REST testing established

--- rest/src/test/java/rest/arquillian/TestTester.java
+++ rest/src/test/java/rest/arquillian/TestTester.java
@@ -1,7 +1,116 @@
 package rest.arquillian;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.File;
+
 /**
  * Created by thofan on 2017-03-15.
  */
+@RunWith(Arquillian.class)
 public class TestTester {
+
+
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    Client client = null;
+    WebTarget target = null;
+    Invocation.Builder invocation = null;
+
+
+    @Before
+    public void before() {
+
+        client = ClientBuilder.newClient();
+        target = client.target("http://fanto.se");
+
+    }
+
+    @After
+    public void after() {
+
+        if (client != null) {
+            client.close();
+        }
+    }
+
+
+
+
+    @Deployment
+    public static WebArchive createDeployment() {
+
+        // TODO this is mostly nonsense but it makes the test run so  we will look into it . . .
+        return ShrinkWrap
+                .create(WebArchive.class)
+                .addPackages(true, "com.library.app")
+                .addAsResource("persistence-integration.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .setWebXML(new File("src/test/resources/web.xml"));
+                /*.addAsLibraries(
+                        Maven.resolver().resolve("com.google.code.gson:gson:2.3.1", "org.mockito:mockito-core:1.9.5")
+                                .withTransitivity().asFile());
+                                */
+    }
+
+
+
+
+    @Test
+    @RunAsClient
+    public void testGETT_alternativ1() {
+
+        invocation = target.request(MediaType.APPLICATION_JSON);
+
+        Response response = invocation.get();
+        String rs = response.readEntity(String.class);
+
+        System.out.println(rs);
+
+    }
+
+    @Test
+    @RunAsClient
+    public void testGET_alternativ2() {
+
+        invocation = target.request(MediaType.APPLICATION_JSON);
+
+        Response response = target.request(MediaType.APPLICATION_JSON).get();
+        String rs = response.readEntity(String.class);
+
+        System.out.println(rs);
+
+    }
+
+    @Test
+    public void testPOST() {
+
+        Form form = new Form();
+        form.param("x", "foo");
+        form.param("y", "bar");
+        String bean = target.request(MediaType.APPLICATION_JSON_TYPE)
+                .post(Entity.entity(form, MediaType.APPLICATION_FORM_URLENCODED_TYPE), String.class);
+    }
+
+
+
+
+
 }
