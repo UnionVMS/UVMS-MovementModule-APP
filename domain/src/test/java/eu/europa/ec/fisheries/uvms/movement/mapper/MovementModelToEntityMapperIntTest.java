a8234b5b92ce6da32c36f9f55d03d71c5730be90
Robert Larsson <robert.larsson@havochvatten.se>
2017-04-05 11:18:15 +0200

    Added mor tests for MovementModelToEntityMapper

--- domain/src/test/java/eu/europa/ec/fisheries/uvms/movement/mapper/MovementModelToEntityMapperIntTest.java
+++ domain/src/test/java/eu/europa/ec/fisheries/uvms/movement/mapper/MovementModelToEntityMapperIntTest.java
@@ -8,18 +8,31 @@ import eu.europa.ec.fisheries.schema.movement.v1.MovementTypeType;
 import eu.europa.ec.fisheries.schema.movement.v1.SegmentCategoryType;
 import eu.europa.ec.fisheries.uvms.movement.arquillian.TransactionalTests;
 import eu.europa.ec.fisheries.uvms.movement.arquillian.bean.util.TestUtil;
+import eu.europa.ec.fisheries.uvms.movement.bean.MovementBatchModelBean;
+import eu.europa.ec.fisheries.uvms.movement.dao.bean.MovementDaoBean;
 import eu.europa.ec.fisheries.uvms.movement.dao.exception.MovementDaoMappingException;
 import eu.europa.ec.fisheries.uvms.movement.entity.Activity;
 import eu.europa.ec.fisheries.uvms.movement.entity.Movement;
+import eu.europa.ec.fisheries.uvms.movement.entity.MovementConnect;
 import eu.europa.ec.fisheries.uvms.movement.entity.Movementmetadata;
+import eu.europa.ec.fisheries.uvms.movement.entity.Segment;
+import eu.europa.ec.fisheries.uvms.movement.entity.Track;
+import eu.europa.ec.fisheries.uvms.movement.exception.GeometryUtilException;
 import eu.europa.ec.fisheries.uvms.movement.model.exception.MovementDaoException;
+import eu.europa.ec.fisheries.uvms.movement.model.exception.MovementDuplicateException;
+import eu.europa.ec.fisheries.uvms.movement.model.exception.MovementModelException;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import static eu.europa.ec.fisheries.uvms.movement.mapper.MovementModelToEntityMapper.createActivity;
@@ -39,6 +52,12 @@ public class MovementModelToEntityMapperIntTest extends TransactionalTests {
     private final static Logger LOG = LoggerFactory.getLogger(MovementModelToEntityMapperIntTest.class);
 
     private TestUtil testUtil = new TestUtil();
+
+    @Inject
+    MovementDaoBean movementDaoBean;
+
+    @Inject
+    MovementBatchModelBean movementBatchModelBean;
 
     @Test
     @OperateOnDeployment("normal")
@@ -234,7 +253,7 @@ public class MovementModelToEntityMapperIntTest extends TransactionalTests {
 
     @Test
     @OperateOnDeployment("normal")
-    public void testCreateActivity() throws MovementDaoException, MovementDaoMappingException {
+    public void testCreateActivity_mapFromMovementBaseTypeToActivity() throws MovementDaoException, MovementDaoMappingException {
 
         //Given
         String uuid = UUID.randomUUID().toString();
@@ -246,6 +265,71 @@ public class MovementModelToEntityMapperIntTest extends TransactionalTests {
         //Then
         assertNotNull(activity);
 
-        LOG.info(" [ testCreateActivity: Mapping from MovementBaseType to Activity was successful. ] ");
+        LOG.info(" [ testCreateActivity: Mapping from MovementBaseType to Activity is successful. ] ");
+    }
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void testCreateSegment_mapFromTwoMovementsToSegment() throws MovementDaoException, MovementDaoMappingException, MovementModelException, MovementDuplicateException, GeometryUtilException {
+
+        //Given: Set up two movements with different timestamps.
+
+        String connectId = UUID.randomUUID().toString();
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(1920, 06, 06);
+        Date date1 = cal.getTime();
+        cal.set(1930, 06, 06);
+        Date date2 = cal.getTime();
+
+        Movement fromMovement = createMovement(0d, 0d, 0d, SegmentCategoryType.EXIT_PORT, connectId, "ONE", date1);
+        Movement toMovement = createMovement(1d, 1d, 0d, SegmentCategoryType.GAP, connectId, "TWO", date2);
+
+        //When
+        Segment segment = MovementModelToEntityMapper.createSegment(fromMovement, toMovement);
+
+        //Then
+        assertNotNull(segment);
+
+        LOG.info(" [ testCreateSegment: Mapping from two Movements to a Segment is successful. ] ");
+    }
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void testCreateTrack_mapFromSegmentToTrack() throws MovementDaoException, MovementDaoMappingException, MovementModelException, MovementDuplicateException, GeometryUtilException {
+
+        //Given: Create a Segment.
+        String connectId = UUID.randomUUID().toString();
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(1920, 06, 06);
+        Date date1 = cal.getTime();
+        cal.set(1930, 06, 06);
+        Date date2 = cal.getTime();
+
+        Movement fromMovement = createMovement(0d, 0d, 0d, SegmentCategoryType.EXIT_PORT, connectId, "ONE", date1);
+        Movement toMovement = createMovement(1d, 1d, 0d, SegmentCategoryType.GAP, connectId, "TWO", date2);
+
+        Segment segment = MovementModelToEntityMapper.createSegment(fromMovement, toMovement);
+
+        //When
+        Track track = MovementModelToEntityMapper.createTrack(segment);
+
+        //Then
+        assertNotNull(track);
+
+        LOG.info(" [ testCreateTrack: Mapping from a Segment to a Track is successful. ] ");
+    }
+
+    private Movement createMovement(double longitude, double latitude, double altitude, SegmentCategoryType segmentCategoryType, String connectId, String userName, Date positionTime) throws MovementModelException, MovementDuplicateException, MovementDaoException {
+        MovementType movementType = testUtil.createMovementType(longitude, latitude, altitude, segmentCategoryType, connectId);
+        movementType.setPositionTime(positionTime);
+        movementType = movementBatchModelBean.createMovement(movementType, userName);
+        em.flush();
+        Assert.assertNotNull(movementType.getConnectId());
+        MovementConnect movementConnect = movementDaoBean.getMovementConnectByConnectId(movementType.getConnectId());
+        List<Movement> movementList = movementConnect.getMovementList();
+        Assert.assertNotNull(movementList);
+        return movementList.get(movementList.size() - 1);
     }
 }
