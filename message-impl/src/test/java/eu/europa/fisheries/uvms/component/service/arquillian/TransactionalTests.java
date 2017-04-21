561967e2c08febae2d9d8fec8f31d532d114d1fa
thomasfanto <thomas.fanto@consid.se>
2017-04-21 11:19:42 +0200

    rollback

--- message-impl/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/TransactionalTests.java
+++ message-impl/src/test/java/eu/europa/fisheries/uvms/component/service/arquillian/TransactionalTests.java
@@ -26,7 +26,7 @@ public class TransactionalTests extends BuildMovementServiceTestDeployment {
 
     @After
     public void after() throws SystemException, HeuristicRollbackException, HeuristicMixedException, RollbackException {
-        userTransaction.commit();
+        userTransaction.rollback();
     }
 
 }
