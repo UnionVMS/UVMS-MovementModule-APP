9996a6973a2d0c27222be0dba17fa7669d276de8
James Sörling <james.p.sorling@consid.se>
2017-09-22 13:04:37 +0200

    update model/events with toStringStyle Short.

--- message/src/main/java/eu/europa/ec/fisheries/uvms/movement/message/event/carrier/EventMessage.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/movement/message/event/carrier/EventMessage.java
@@ -12,6 +12,10 @@ copy of the GNU General Public License along with the IFDM Suite. If not, see <h
 package eu.europa.ec.fisheries.uvms.movement.message.event.carrier;
 
 import javax.jms.TextMessage;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 /**
  **/
@@ -43,6 +47,21 @@ public class EventMessage {
 
     public void setJmsMessage(TextMessage jmsMessage) {
         this.jmsMessage = jmsMessage;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        return EqualsBuilder.reflectionEquals(this, that);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
     }
 
 }
