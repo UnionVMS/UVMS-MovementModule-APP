2a4fc8f315f07fe514be42e945cc2f3188df7459
Kasim Gul <kasimgul@gmail.com>
2018-09-27 11:24:31 +0200

    Removing Interface & Refactoring

--- service/src/main/java/eu/europa/ec/fisheries/uvms/movement/service/bean/MovementSearchGroupService.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/movement/service/bean/MovementSearchGroupService.java
@@ -11,12 +11,10 @@ copy of the GNU General Public License along with the IFDM Suite. If not, see <h
  */
 package eu.europa.ec.fisheries.uvms.movement.service.bean;
 
-import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import eu.europa.ec.fisheries.schema.movement.search.v1.MovementSearchGroup;
-import eu.europa.ec.fisheries.uvms.movement.service.MovementSearchGroupService;
 import eu.europa.ec.fisheries.uvms.movement.service.dao.MovementSearchGroupDao;
 import eu.europa.ec.fisheries.uvms.movement.service.entity.group.MovementFilterGroup;
 import eu.europa.ec.fisheries.uvms.movement.service.exception.ErrorCode;
@@ -26,13 +24,12 @@ import eu.europa.ec.fisheries.uvms.movement.service.mapper.MovementGroupMapper;
 import eu.europa.ec.fisheries.uvms.movement.service.validation.MovementGroupValidator;
 
 @Stateless
-public class MovementSearchGroupServiceBean implements MovementSearchGroupService {
+public class MovementSearchGroupService {
 
     @Inject
     private MovementSearchGroupDao dao;
-    
-    @Override
-    public MovementSearchGroup createMovementSearchGroup(MovementSearchGroup searchGroup, String username) throws MovementServiceException {
+
+    public MovementFilterGroup createMovementFilterGroup(MovementSearchGroup searchGroup, String username) throws MovementServiceException {
         if(searchGroup.getName() == null || searchGroup.getName().isEmpty()){
             throw new MovementServiceRuntimeException("Search group must have a name", ErrorCode.ILLEGAL_ARGUMENT_ERROR);
         }
@@ -42,8 +39,7 @@ public class MovementSearchGroupServiceBean implements MovementSearchGroupServic
         try {
             if (MovementGroupValidator.isMovementGroupOk(searchGroup)) {
                 MovementFilterGroup filterGroup = MovementGroupMapper.toGroupEntity(searchGroup, username);
-                filterGroup = dao.createMovementFilterGroup(filterGroup);
-                return MovementGroupMapper.toMovementSearchGroup(filterGroup);
+                return dao.createMovementFilterGroup(filterGroup);
             } else {
                 throw new MovementServiceException("One or several movement types are misspelled or non existent." +
                         " Allowed values are: [ " + MovementGroupValidator.ALLOWED_FIELD_VALUES + " ]", ErrorCode.UNSUCCESSFUL_DB_OPERATION);
@@ -53,60 +49,50 @@ public class MovementSearchGroupServiceBean implements MovementSearchGroupServic
         }
     }
 
-    @Override
-    public MovementSearchGroup getMovementSearchGroup(Long id) throws MovementServiceException {
+    public MovementFilterGroup getMovementFilterGroup(Long id) throws MovementServiceException {
         try {
             MovementFilterGroup filterGroup = dao.getMovementFilterGroupById(id.intValue());
             if (filterGroup == null) {
                 throw new MovementServiceRuntimeException("Could not get movement search group by group ID:"
                         + id.intValue(), ErrorCode.ILLEGAL_ARGUMENT_ERROR);
             }
-            return MovementGroupMapper.toMovementSearchGroup(filterGroup);
+            return filterGroup;
         } catch (Exception e) {
             throw new MovementServiceException("Error when getting movement search group", e, ErrorCode.DATA_RETRIEVING_ERROR);
         }
     }
 
-    @Override
-    public List<MovementSearchGroup> getMovementSearchGroupsByUser(String user) throws MovementServiceException {
+    public List<MovementFilterGroup> getMovementFilterGroupsByUser(String user) throws MovementServiceException {
         try {
-            List<MovementFilterGroup> filterGroups = dao.getMovementFilterGroupsByUser(user);
-            List<MovementSearchGroup> searchGroups = new ArrayList<>();
-            for (MovementFilterGroup filterGroup : filterGroups) {
-                searchGroups.add(MovementGroupMapper.toMovementSearchGroup(filterGroup));
-            }
-            return searchGroups;
+            return dao.getMovementFilterGroupsByUser(user);
         } catch (Exception e) {
             throw new MovementServiceException("Error when getting movement search groups by user", e, ErrorCode.DATA_RETRIEVING_ERROR);
         }
     }
 
-    @Override
-    public MovementSearchGroup updateMovementSearchGroup(MovementSearchGroup searchGroup, String username) throws MovementServiceException {
-        if(searchGroup.getId() == null || username == null){
+    public MovementFilterGroup updateMovementFilterGroup(MovementSearchGroup updatedDTO, String username) throws MovementServiceException {
+        if(updatedDTO.getId() == null || username == null){
             throw new MovementServiceRuntimeException("Error when updating movement search group." +
                     " MovementSearchGroup has no id set or the username is null", ErrorCode.ILLEGAL_ARGUMENT_ERROR);
         }
         try {
-            MovementFilterGroup currentGroup = dao.getMovementFilterGroupById(searchGroup.getId().intValue());
-            if(!currentGroup.getUser().equalsIgnoreCase(searchGroup.getUser())){
-                throw new MovementServiceException("Could not update movement search groups", ErrorCode.UNSUCCESSFUL_DB_OPERATION);
+            MovementFilterGroup currentGroup = dao.getMovementFilterGroupById(updatedDTO.getId().intValue());
+            if(!currentGroup.getUser().equalsIgnoreCase(updatedDTO.getUser())){
+                throw new MovementServiceException("Could not update movement search groups", ErrorCode.INVALID_USERNAME);
             }
 
-            currentGroup = MovementGroupMapper.toGroupEntity(currentGroup, searchGroup, username);
-            MovementFilterGroup updatedGroup = dao.updateMovementFilterGroup(currentGroup);
-            return MovementGroupMapper.toMovementSearchGroup(updatedGroup);
+            currentGroup = MovementGroupMapper.toGroupEntity(currentGroup, updatedDTO, username);
+            return dao.updateMovementFilterGroup(currentGroup);
         } catch (MovementServiceException e) {
             throw new MovementServiceException("Error when updating movement search group.", e, ErrorCode.UNSUCCESSFUL_DB_OPERATION);
         }
     }
 
-    @Override
-    public MovementSearchGroup deleteMovementSearchGroup(Long id) throws MovementServiceException {
+    public MovementFilterGroup deleteMovementFilterGroup(Long id) throws MovementServiceException {
         try {
             MovementFilterGroup filterGroup = dao.getMovementFilterGroupById(id.intValue());
             filterGroup = dao.deleteMovementFilterGroup(filterGroup);
-            return MovementGroupMapper.toMovementSearchGroup(filterGroup);
+            return filterGroup;
         } catch (Exception e) {
             throw new MovementServiceException("Error when deleting movement search group", e, ErrorCode.UNSUCCESSFUL_DB_OPERATION);
         }
