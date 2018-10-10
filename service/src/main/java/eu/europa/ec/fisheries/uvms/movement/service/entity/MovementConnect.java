aa547511c61ca1d1f0e2aa10d03fceb79d9d4fbb
Kasim Gul <kasimgul@gmail.com>
2018-10-10 16:55:37 +0200

    Adding indexs to entities

--- service/src/main/java/eu/europa/ec/fisheries/uvms/movement/service/entity/MovementConnect.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/movement/service/entity/MovementConnect.java
@@ -23,26 +23,15 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.List;
 
-import javax.persistence.Basic;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- **/
 @Entity
-@Table(name = "movementconnect")
+@Table(name = "movementconnect", uniqueConstraints = {
+        @UniqueConstraint(name = "moveconn_value_unique", columnNames = "moveconn_value")
+})
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = MovementConnect.MOVEMENT_CONNECT_BY_CONNECT_ID, query = "SELECT m FROM MovementConnect m WHERE m.value = :value"),
