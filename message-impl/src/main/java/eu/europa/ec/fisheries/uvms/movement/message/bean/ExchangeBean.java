8a94d50e4901b68189581f12f1111a097c2672b1
peerik <peter.eriksson@havochvatten.se>
2018-11-21 10:20:49 +0100

    Set method in ProcessedMovementResponse

--- message-impl/src/main/java/eu/europa/ec/fisheries/uvms/movement/message/bean/ExchangeBean.java
+++ message-impl/src/main/java/eu/europa/ec/fisheries/uvms/movement/message/bean/ExchangeBean.java
@@ -1,5 +1,7 @@
 package eu.europa.ec.fisheries.uvms.movement.message.bean;
 
+import javax.ejb.Stateless;
+import eu.europa.ec.fisheries.schema.exchange.module.v1.ExchangeModuleMethod;
 import eu.europa.ec.fisheries.schema.exchange.module.v1.ProcessedMovementResponse;
 import eu.europa.ec.fisheries.uvms.commons.message.api.MessageConstants;
 import eu.europa.ec.fisheries.uvms.commons.message.api.MessageException;
@@ -7,18 +9,12 @@ import eu.europa.ec.fisheries.uvms.commons.message.impl.AbstractProducer;
 import eu.europa.ec.fisheries.uvms.exchange.model.exception.ExchangeModelMarshallException;
 import eu.europa.ec.fisheries.uvms.exchange.model.mapper.JAXBMarshaller;
 
-import javax.annotation.Resource;
-import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSConnectionFactory;
-import javax.jms.JMSContext;
-import javax.jms.Queue;
-
 @Stateless
 public class ExchangeBean extends AbstractProducer {
 
     public void send(ProcessedMovementResponse processedMovementResponse) throws ExchangeModelMarshallException, MessageException {
+        processedMovementResponse.setMethod(ExchangeModuleMethod.PROCESSED_MOVEMENT);
+        processedMovementResponse.setUsername("");
         String xml = JAXBMarshaller.marshallJaxBObjectToString(processedMovementResponse);
         sendModuleMessage(xml, null);
     }
