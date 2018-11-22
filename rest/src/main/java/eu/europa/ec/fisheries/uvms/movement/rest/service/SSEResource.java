69b34253ae4b672ae16f37682a6749552c079238
Jonas E.Mattsson <jonas.mattsson@consid.se>
2018-11-22 12:49:40 +0100

    Auth filter works :)

--- rest/src/main/java/eu/europa/ec/fisheries/uvms/movement/rest/service/SSEResource.java
+++ rest/src/main/java/eu/europa/ec/fisheries/uvms/movement/rest/service/SSEResource.java
@@ -7,7 +7,13 @@ import eu.europa.ec.fisheries.uvms.movement.service.dao.MovementDao;
 import eu.europa.ec.fisheries.uvms.movement.service.dto.MicroMovementDto;
 import eu.europa.ec.fisheries.uvms.movement.service.entity.Movement;
 import eu.europa.ec.fisheries.uvms.movement.service.event.CreatedMovement;
+import eu.europa.ec.fisheries.uvms.rest.security.RequiresFeature;
+import eu.europa.ec.fisheries.uvms.rest.security.UnionVMSFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.net.www.http.KeepAliveStream;
 
+import javax.ejb.Stateless;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
@@ -25,7 +31,10 @@ import java.util.UUID;
 
 @ApplicationScoped
 @Path("sse")
+@RequiresFeature(UnionVMSFeature.viewMovements)
 public class SSEResource {
+
+    private final static Logger LOG = LoggerFactory.getLogger(SSEResource.class);
 
     @Inject
     MovementDao movementDao;
@@ -42,19 +51,24 @@ public class SSEResource {
     }
 
     public void createdMovement(@Observes @CreatedMovement NotificationMessage message){
-        UUID guid = (UUID) message.getProperties().get(LongPollingConstants.MOVEMENT_GUID_KEY);
-        Movement move = movementDao.getMovementByGUID(guid);
-        if (move != null) {
-            MicroMovementDto micro = new MicroMovementDto(move.getLocation(), move.getHeading(), move.getGuid(), move.getMovementConnect(), move.getTimestamp(), move.getSpeed());
-            OutboundSseEvent sseEvent = eventBuilder
-                    .name("Movement")
-                    .id(String.valueOf(guid))
-                    .mediaType(MediaType.APPLICATION_JSON_TYPE)
-                    .data(MicroMovementDto.class, micro)
-                    //.reconnectDelay(3000) //this one is optional and governs how long the client should wait b4 attempting to reconnect to this server
-                    .comment("New Movement")
-                    .build();
-            sseBroadcaster.broadcast(sseEvent);
+        try {
+            UUID guid = (UUID) message.getProperties().get(LongPollingConstants.MOVEMENT_GUID_KEY);
+            Movement move = movementDao.getMovementByGUID(guid);
+            if (move != null) {
+                MicroMovementDto micro = new MicroMovementDto(move.getLocation(), move.getHeading(), move.getGuid(), move.getMovementConnect(), move.getTimestamp(), move.getSpeed());
+                OutboundSseEvent sseEvent = eventBuilder
+                        .name("Movement")
+                        .id("" + System.currentTimeMillis())
+                        .mediaType(MediaType.APPLICATION_JSON_PATCH_JSON_TYPE)
+                        .data(MicroMovementDto.class, micro)
+                        //.reconnectDelay(3000) //this one is optional and governs how long the client should wait b4 attempting to reconnect to this server
+                        .comment("New Movement")
+                        .build();
+                sseBroadcaster.broadcast(sseEvent);
+            }
+        }catch (Exception e){
+            LOG.error("Bad stuffz: ", e);
+            throw new RuntimeException(e);
         }
     }
 
