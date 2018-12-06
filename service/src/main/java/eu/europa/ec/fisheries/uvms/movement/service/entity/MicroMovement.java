f747878451feed78657aae7fe8ce96e64e2c2905
Jonas E.Mattsson <jonas.mattsson@consid.se>
2018-12-06 14:07:18 +0100

    added flagstate and asset name

--- service/src/main/java/eu/europa/ec/fisheries/uvms/movement/service/entity/MicroMovement.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/movement/service/entity/MicroMovement.java
@@ -23,87 +23,103 @@ import java.time.Instant;
 public class MicroMovement implements Serializable{
 
 
-        @NotNull
-        @Type(type = "org.hibernate.spatial.GeometryType")
-        @Column(name = "move_location", columnDefinition = "Geometry")
-        private Point location;
+    @NotNull
+    @Type(type = "org.hibernate.spatial.GeometryType")
+    @Column(name = "move_location", columnDefinition = "Geometry")
+    private Point location;
 
 
-        @Column(name = "move_heading")
-        private Double heading;
+    @Column(name = "move_heading")
+    private Double heading;
 
-        @NotNull
-        @Id
-        @Size(max = 36)
-        @Column(name = "move_id", nullable = false)
-        private String guid;
+    @NotNull
+    @Id
+    @Size(max = 36)
+    @Column(name = "move_id", nullable = false)
+    private String guid;
 
-        @NotNull
-        @Fetch(FetchMode.JOIN)
-        @JoinColumn(name = "move_moveconn_id", referencedColumnName = "moveconn_asset_id")
-        @ManyToOne(cascade = CascadeType.PERSIST)
-        private MovementConnect movementConnect;
+    @NotNull
+    @Fetch(FetchMode.JOIN)
+    @JoinColumn(name = "move_moveconn_id", referencedColumnName = "moveconn_asset_id")
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private MovementConnect movementConnect;
 
-        @JsonSerialize(using = InstantSerializer.class)
-        @JsonDeserialize(using = MovementInstantDeserializer.class)
-        @Column(name = "move_timestamp")
-        private Instant timestamp;
+    @JsonSerialize(using = InstantSerializer.class)
+    @JsonDeserialize(using = MovementInstantDeserializer.class)
+    @Column(name = "move_timestamp")
+    private Instant timestamp;
 
-        @Column(name = "move_speed")
-        private Double speed;
+    @Column(name = "move_speed")
+    private Double speed;
 
-        @Column(name = "moveconn_flagstate")
-        private String flagstate;
+    @Column(name = "moveconn_flagstate")
+    private String flagstate;
 
-        @Column(name = "moveconn_name")
-        private String name;
+    @Column(name = "moveconn_name")
+    private String assetName;
 
 
-        public Point getLocation() {
-            return location;
-        }
+    public Point getLocation() {
+        return location;
+    }
 
-        public void setLocation(Point location) {
-            this.location = location;
-        }
+    public void setLocation(Point location) {
+        this.location = location;
+    }
 
-        public Double getHeading() {
-            return heading;
-        }
+    public Double getHeading() {
+        return heading;
+    }
 
-        public void setHeading(Double heading) {
-            this.heading = heading;
-        }
+    public void setHeading(Double heading) {
+        this.heading = heading;
+    }
 
-        public String getGuid() {
-            return guid;
-        }
+    public String getGuid() {
+        return guid;
+    }
 
-        public void setGuid(String guid) {
-            this.guid = guid;
-        }
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
 
-        public MovementConnect getMovementConnect() {
-            return movementConnect;
-        }
+    public MovementConnect getMovementConnect() {
+        return movementConnect;
+    }
 
-        public void setMovementConnect(MovementConnect movementConnect) {
-            this.movementConnect = movementConnect;
-        }
+    public void setMovementConnect(MovementConnect movementConnect) {
+        this.movementConnect = movementConnect;
+    }
 
-        public Instant getTimestamp() {
-            return timestamp;
-        }
+    public Instant getTimestamp() {
+        return timestamp;
+    }
 
-        public void setTimestamp(Instant timestamp) {
-            this.timestamp = timestamp;
-        }
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
 
-        public Double getSpeed() {
-            return speed;
-        }
+    public Double getSpeed() {
+        return speed;
+    }
 
-        public void setSpeed(Double speed) {
-            this.speed = speed;
-        }
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+    public String getFlagstate() {
+        return flagstate;
+    }
+
+    public void setFlagstate(String flagstate) {
+        this.flagstate = flagstate;
+    }
+
+    public String getAssetName() {
+        return assetName;
+    }
+
+    public void setAssetName(String assetName) {
+        this.assetName = assetName;
+    }
 }
