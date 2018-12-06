3565e8c5a4201cc5a1897bad65297e5200d9128b
Jonas E.Mattsson <jonas.mattsson@consid.se>
2018-12-06 13:30:55 +0100

    test fix

--- service/src/test/java/eu/europa/ec/fisheries/uvms/movement/service/dao/AlarmDaoIntTest.java
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/movement/service/dao/AlarmDaoIntTest.java
@@ -1,26 +1,19 @@
 package eu.europa.ec.fisheries.uvms.movement.service.dao;
 
-import eu.europa.ec.fisheries.schema.exchange.plugin.types.v1.PluginType;
 import eu.europa.ec.fisheries.uvms.movement.service.TransactionalTests;
-import eu.europa.ec.fisheries.uvms.movement.service.dto.AlarmStatusType;
 import eu.europa.ec.fisheries.uvms.movement.service.entity.IncomingMovement;
 import eu.europa.ec.fisheries.uvms.movement.service.entity.alarm.AlarmItem;
 import eu.europa.ec.fisheries.uvms.movement.service.entity.alarm.AlarmReport;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import javax.ejb.EJB;
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.UUID;
 
-import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(Arquillian.class)
@@ -28,40 +21,6 @@ public class AlarmDaoIntTest extends TransactionalTests {
 
     @EJB
     private AlarmDAO alarmDAO;
-
-    @Test
-    @OperateOnDeployment("movementservice")
-    public void basicAlarmPersistTest(){
-        AlarmReport alarmReport;
-        alarmReport = new AlarmReport();
-        alarmReport.setAssetGuid(UUID.randomUUID().toString());
-        alarmReport.setCreatedDate(Instant.now());
-        alarmReport.setPluginType(PluginType.MANUAL.value());
-        //alarmReport.setRecipient();
-        alarmReport.setStatus(AlarmStatusType.OPEN.value());
-        alarmReport.setUpdated(Instant.now());
-        alarmReport.setUpdatedBy("UVMS");
-        alarmReport.setIncomingMovement(null);
-        alarmReport.setAlarmItemList(new ArrayList<>());
-        alarmDAO.save(alarmReport);
-
-        assertNotNull(alarmReport.getId());
-
-        AlarmItem item = new AlarmItem();
-        //alarmReport.getAlarmItemList().add(item);
-        item.setAlarmReport(alarmReport);
-        item.setRuleGuid("Test rules"); // WTF?
-        item.setRuleName("Test rules");
-        item.setUpdated(Instant.now());
-        item.setUpdatedBy("UVMS");
-        alarmDAO.save(item);
-
-        assertNotNull(item.getId());
-
-        AlarmReport ar = alarmDAO.getAlarmReportByGuid(alarmReport.getId());
-
-        assertTrue(ar.getAlarmItemList().size() > 0);
-    }
 
     @Test
     @OperateOnDeployment("movementservice")
