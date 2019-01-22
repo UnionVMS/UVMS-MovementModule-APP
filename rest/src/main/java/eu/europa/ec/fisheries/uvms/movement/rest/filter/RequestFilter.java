606de2213d1b6f2ecae9706b5057206e881bfb59
HAV\thofan <thomas.fanto@havochvatten.se>
2019-01-22 12:32:57 +0100

    removed old ugly version

--- rest/src/main/java/eu/europa/ec/fisheries/uvms/movement/rest/filter/RequestFilter.java
+++ rest/src/main/java/eu/europa/ec/fisheries/uvms/movement/rest/filter/RequestFilter.java
@@ -45,18 +45,6 @@ public class RequestFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 
-        // TODO MDC is prototyping TEST
-        // TODO if we want to put the entire request in a db-table with for instance requestId as key (to be able to fast and easy reproduce a reported error)
-        // TODO we must do some things first since it will "consume" the stream and make it unusable at a later stage (the rest of the application)
-        // TODO read this article http://stackoverflow.com/questions/10210645/http-servlet-request-lose-params-from-post-body-after-read-it-once
-        try {
-            String val = MDC.get("requestId");
-            if(val == null || val.trim().length()< 1){
-                MDC.put("requestId", UUID.randomUUID().toString());
-            }
-        }catch(IllegalArgumentException e){
-            LOG.error("MDC init error. App works but tracing will be unpredictible", e);
-        }
 
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader(RestConstants.ACCESS_CONTROL_ALLOW_ORIGIN, RestConstants.ACCESS_CONTROL_ALLOW_METHODS_ALL);
