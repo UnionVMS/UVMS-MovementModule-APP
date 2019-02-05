3608393cfda27cc2814d4149c3fda382536c3dfe
Jonas E.Mattsson <jonas.mattsson@consid.se>
2019-02-05 12:56:05 +0100

    removed the index on movement source

--- service/src/main/java/eu/europa/ec/fisheries/uvms/movement/service/entity/Movement.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/movement/service/entity/Movement.java
@@ -39,7 +39,6 @@ import org.hibernate.annotations.Type;
         @Index(columnList = "move_act_id", name = "movement_act_fk_idx", unique = false),
         @Index(columnList = "move_duplicate", name = "movement_duplicate_idx", unique = false),
         @Index(columnList = "move_moveconn_id", name = "movement_moveconn_fk_idx", unique = false),
-        @Index(columnList = "move_movesour_id", name = "movement_movesour_fk_idx", unique = false),
         @Index(columnList = "move_processed", name = "movement_processed_idx", unique = false),
         @Index(columnList = "move_timestamp", name = "movement_timestamp_idx", unique = false),
         @Index(columnList = "move_trac_id", name = "movement_trac_fk_idx", unique = false)
