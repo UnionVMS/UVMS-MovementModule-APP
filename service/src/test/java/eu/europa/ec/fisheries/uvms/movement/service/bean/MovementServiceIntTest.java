404e2e5ba8585b1edd14a076403281277768f076
peerik <peter.eriksson@havochvatten.se>
2019-03-08 11:16:29 +0100

    Merged tests

--- service/src/test/java/eu/europa/ec/fisheries/uvms/movement/service/bean/MovementServiceIntTest.java
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/movement/service/bean/MovementServiceIntTest.java
@@ -2,6 +2,7 @@ package eu.europa.ec.fisheries.uvms.movement.service.bean;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -34,6 +35,7 @@ import eu.europa.ec.fisheries.uvms.movement.service.MockData;
 import eu.europa.ec.fisheries.uvms.movement.service.TransactionalTests;
 import eu.europa.ec.fisheries.uvms.movement.service.entity.LatestMovement;
 import eu.europa.ec.fisheries.uvms.movement.service.entity.Movement;
+import eu.europa.ec.fisheries.uvms.movement.service.entity.MovementConnect;
 
 @RunWith(Arquillian.class)
 public class MovementServiceIntTest extends TransactionalTests {
@@ -325,6 +327,55 @@ public class MovementServiceIntTest extends TransactionalTests {
         } catch (Exception e) {
             assertNotNull(e);
         }
+    }
+    
+    @Test
+    @OperateOnDeployment("movementservice")
+    public void getMovementConnect() {
+        // Note getOrCreateMovementConnectByConnectId CREATES one if it does not exists  (probably to force a batch import to succeed)
+        MovementConnect mc = new MovementConnect();
+        mc.setId(UUID.randomUUID());
+        mc.setUpdated(Instant.now());
+        mc.setUpdatedBy("Test Connector");
+        MovementConnect fetchedMovementConnect = movementService.getOrCreateMovementConnectByConnectId(mc);
+        assertNotNull(fetchedMovementConnect);
+        assertEquals(fetchedMovementConnect.getId(), mc.getId());
+    }
+
+
+    @Test
+    @OperateOnDeployment("movementservice")
+    public void getMovementConnect_ZEROISH_GUID() {
+        MovementConnect mc = new MovementConnect();
+        mc.setId(UUID.fromString("100000-0000-0000-0000-000000000000"));
+        mc.setUpdated(Instant.now());
+        mc.setUpdatedBy("Test Connector");
+        // Note getOrCreateMovementConnectByConnectId CREATES one if it does not exists  (probably to force a batchimport to succeed)
+        MovementConnect fetchedMovementConnect = movementService.getOrCreateMovementConnectByConnectId(mc);
+        assertNotNull(fetchedMovementConnect);
+        assertEquals(fetchedMovementConnect.getId(), mc.getId());
+    }
+
+    @Test
+    @OperateOnDeployment("movementservice")
+    public void getMovementConnect_NULL_GUID() {
+        assertNull(movementService.getOrCreateMovementConnectByConnectId(null));
+    }
+
+    @Test
+    @OperateOnDeployment("movementservice")
+    public void createMovement2() {
+        double longitude = rnd.nextDouble();
+        double latitude = rnd.nextDouble();
+
+        UUID randomUUID = UUID.randomUUID();
+        Movement movement = MockData.createMovement(longitude, latitude, randomUUID);
+        movement.getMovementConnect().setId(randomUUID);
+
+        Movement created = movementService.createMovement(movement);
+        assertNotNull(created);
+        assertEquals(randomUUID, created.getMovementConnect().getId());
+
     }
 
     /******************************************************************************************************************
