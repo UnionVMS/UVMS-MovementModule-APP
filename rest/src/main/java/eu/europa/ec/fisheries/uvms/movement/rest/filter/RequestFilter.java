1dcbddc804a0741c8b753c8f7663ae145032d0ed
Kasim Gul <kasimgul@gmail.com>
2019-10-28 09:06:36 +0100

    Updating cors filter

--- rest/src/main/java/eu/europa/ec/fisheries/uvms/movement/rest/filter/RequestFilter.java
+++ rest/src/main/java/eu/europa/ec/fisheries/uvms/movement/rest/filter/RequestFilter.java
@@ -20,7 +20,6 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.ForbiddenException;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -32,7 +31,7 @@ public class RequestFilter implements Filter {
 
     /**
      * {@code corsOriginRegex} is valid for given host/origin names/IPs and any range of sub domains.
-     *
+     * <p>
      * localhost:[2]8080
      * 127.0.0.1:[2]8080
      * 192.168.***.***:[2]8080
@@ -51,30 +50,26 @@ public class RequestFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        final String HOST = httpServletRequest.getHeader("ORIGIN");
+        String origin = httpServletRequest.getHeader("ORIGIN");
 
-        boolean isValid = validateHost(HOST);
+        if(origin != null && validateOrigin(origin)) {
+            HttpServletResponse response = (HttpServletResponse) res;
+            response.setHeader(RestConstants.ACCESS_CONTROL_ALLOW_ORIGIN, origin);
+            response.setHeader(RestConstants.ACCESS_CONTROL_ALLOW_METHODS, RestConstants.ACCESS_CONTROL_ALLOWED_METHODS);
+            response.setHeader(RestConstants.ACCESS_CONTROL_ALLOW_HEADERS, RestConstants.ACCESS_CONTROL_ALLOW_HEADERS_ALL);
 
-        if (!isValid)
-            throw new ForbiddenException("You are not allowed to make any request from an external domain. Your Host: " + HOST);
-
-        HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader(RestConstants.ACCESS_CONTROL_ALLOW_ORIGIN, HOST);
-        response.setHeader(RestConstants.ACCESS_CONTROL_ALLOW_METHODS, RestConstants.ACCESS_CONTROL_ALLOWED_METHODS);
-        response.setHeader(RestConstants.ACCESS_CONTROL_ALLOW_HEADERS, RestConstants.ACCESS_CONTROL_ALLOW_HEADERS_ALL);
-
-        if (httpServletRequest.getMethod().equals("OPTIONS")) {
-            response.setStatus(200);
-            return;
+            if (httpServletRequest.getMethod().equals("OPTIONS")) {
+                response.setStatus(200);
+                return;
+            }
         }
-
         chain.doFilter(request, res);
     }
 
 
-    private boolean validateHost(String host) {
+    private boolean validateOrigin(String origin) {
         Pattern pattern = Pattern.compile(corsOriginRegex);
-        Matcher matcher = pattern.matcher(host);
+        Matcher matcher = pattern.matcher(origin);
         return matcher.matches();
     }
 
