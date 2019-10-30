5d4b9651557b1f3446108388c9c9b3f654455aff
Jonas E.Mattsson <jonas.mattsson@consid.se>
2019-10-30 16:47:15 +0100

    refactor to use jms context

--- service/src/main/java/eu/europa/ec/fisheries/uvms/movement/service/message/EventStreamSender.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/movement/service/message/EventStreamSender.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import eu.europa.ec.fisheries.uvms.commons.message.api.MessageConstants;
 import eu.europa.ec.fisheries.uvms.commons.message.context.MappedDiagnosticContext;
+import eu.europa.ec.fisheries.uvms.commons.message.impl.AbstractTopicProducer;
 import eu.europa.ec.fisheries.uvms.movement.service.dto.MicroMovementExtended;
 import eu.europa.ec.fisheries.uvms.movement.service.entity.Movement;
 import eu.europa.ec.fisheries.uvms.movement.service.event.CreatedMovement;
@@ -13,35 +14,30 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import javax.ejb.Stateless;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.enterprise.event.TransactionPhase;
+import javax.inject.Inject;
 import javax.jms.*;
+import javax.transaction.Transactional;
 
-@ApplicationScoped
-public class EventStreamSender {
+@Stateless
+public class EventStreamSender /*extends AbstractTopicProducer*/ {
 
     private final static Logger LOG = LoggerFactory.getLogger(EventStreamSender.class);
 
     @Resource(mappedName = "java:/" + MessageConstants.EVENT_STREAM_TOPIC)
     private Destination destination;
 
+    @Inject
+    @JMSConnectionFactory("java:/ConnectionFactory")
+    JMSContext context;
+
     private ObjectMapper om = new ObjectMapper();
 
-    @Resource(mappedName = "java:/ConnectionFactory")
-    private ConnectionFactory connectionFactory;
-
-    private Session session;
-    private MessageProducer messageProducer;
-
     @PostConstruct
-    public void init() throws Exception {
-        Connection connection = connectionFactory.createConnection();
-        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        messageProducer = session.createProducer(destination);
-        messageProducer.setDeliveryMode(1);
-        messageProducer.setTimeToLive(5000L);
-
+    public void init() {
         om.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         om.setSerializationInclusion(JsonInclude.Include.NON_NULL);
     }
@@ -53,12 +49,12 @@ public class EventStreamSender {
                         move.getHeading(), move.getId(), move.getMovementConnect().getId(), move.getTimestamp(), move.getSpeed(), move.getMovementSource());
                 String outgoingJson = om.writeValueAsString(micro);
 
-                TextMessage message = session.createTextMessage(outgoingJson);
+                TextMessage message = this.context.createTextMessage(outgoingJson);
                 message.setStringProperty("event", "Movement");
                 message.setStringProperty("subscribers", null);
                 MappedDiagnosticContext.addThreadMappedDiagnosticContextToMessageProperties(message);
 
-                messageProducer.send(message);
+                context.createProducer().setDeliveryMode(1).setTimeToLive(5000L).send(destination, message);
 
             }
         }catch (Exception e){
