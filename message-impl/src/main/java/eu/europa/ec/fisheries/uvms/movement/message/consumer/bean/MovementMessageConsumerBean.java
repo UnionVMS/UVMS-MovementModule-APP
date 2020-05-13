8dd28b630de83f0dbce20965d83f7e764988a5ba
nomikosi <42890529+nomikosi@users.noreply.github.com>
2020-05-13 14:59:53 +0300

    [stability-fixes] JMS messages are sent with JMSXGroupID option, added multiple workers

--- message-impl/src/main/java/eu/europa/ec/fisheries/uvms/movement/message/consumer/bean/MovementMessageConsumerBean.java
+++ message-impl/src/main/java/eu/europa/ec/fisheries/uvms/movement/message/consumer/bean/MovementMessageConsumerBean.java
@@ -11,18 +11,15 @@
  */
 package eu.europa.ec.fisheries.uvms.movement.message.consumer.bean;
 
-import javax.ejb.ActivationConfigProperty;
-import javax.ejb.MessageDriven;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
+import eu.europa.ec.fisheries.uvms.commons.message.context.PropagateFluxEnvelopeData;
 import eu.europa.ec.fisheries.schema.movement.module.v1.MovementBaseRequest;
 import eu.europa.ec.fisheries.schema.movement.module.v1.MovementModuleMethod;
-import eu.europa.ec.fisheries.uvms.commons.message.api.MessageConstants;
-import eu.europa.ec.fisheries.uvms.commons.message.context.PropagateFluxEnvelopeData;
 import eu.europa.ec.fisheries.uvms.movement.message.event.ErrorEvent;
 import eu.europa.ec.fisheries.uvms.movement.message.event.carrier.EventMessage;
 import eu.europa.ec.fisheries.uvms.movement.model.exception.MovementModelException;
@@ -31,17 +28,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-@MessageDriven(mappedName = MessageConstants.QUEUE_MODULE_MOVEMENT, activationConfig = {
-        @ActivationConfigProperty(propertyName = MessageConstants.MESSAGING_TYPE_STR, propertyValue = MessageConstants.CONNECTION_TYPE),
-        @ActivationConfigProperty(propertyName = MessageConstants.DESTINATION_TYPE_STR, propertyValue = MessageConstants.DESTINATION_TYPE_QUEUE),
-        @ActivationConfigProperty(propertyName = MessageConstants.DESTINATION_STR, propertyValue = MessageConstants.COMPONENT_MESSAGE_IN_QUEUE),
-        @ActivationConfigProperty(propertyName = MessageConstants.DESTINATION_JNDI_NAME, propertyValue = MessageConstants.QUEUE_MODULE_MOVEMENT),
-        @ActivationConfigProperty(propertyName = MessageConstants.CONNECTION_FACTORY_JNDI_NAME, propertyValue = MessageConstants.CONNECTION_FACTORY),
-        @ActivationConfigProperty(propertyName = "maxMessagesPerSessions", propertyValue = "100"),
-        @ActivationConfigProperty(propertyName = "initialRedeliveryDelay", propertyValue = "60000"),
-        @ActivationConfigProperty(propertyName = "maximumRedeliveries", propertyValue = "3"),
-        @ActivationConfigProperty(propertyName = "maxSessions", propertyValue = "10")
-})
 public class MovementMessageConsumerBean implements MessageListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(MovementMessageConsumerBean.class);
@@ -104,7 +90,7 @@ public class MovementMessageConsumerBean implements MessageListener {
                     errorEvent.fire(new EventMessage(textMessage, "[ Request method " + movementMethod.name() + "  is not implemented ]"));
             }
         } catch (NullPointerException | ClassCastException | MovementModelException e) {
-            LOG.error("[ Error when receiving message in movement: ] {}", e);
+            LOG.error("[ Error when receiving message in movement: ]", e);
             errorEvent.fire(new EventMessage(textMessage, "Error when receiving message in movement: " + e.getMessage()));
         }
     }
