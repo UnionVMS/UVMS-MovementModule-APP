fda39a3ff1fe8a6ba349cc6aa6808f5303f10947
peerik <peter.eriksson@havochvatten.se>
2020-08-20 12:09:25 +0200

    cleanup

--- client/src/main/java/eu/europa/ec/fisheries/uvms/movement/client/MovementRestClient.java
+++ client/src/main/java/eu/europa/ec/fisheries/uvms/movement/client/MovementRestClient.java
@@ -121,7 +121,6 @@ public class MovementRestClient {
                 .request(MediaType.APPLICATION_JSON)
                 .header(HttpHeaders.AUTHORIZATION, internalRestTokenHandler.createAndFetchToken("user"))
                 .post(Entity.entity(cursorPagination, MediaType.APPLICATION_JSON_TYPE), String.class);
-        System.out.println(response);
         return jsonb.fromJson(response, new ArrayList<MovementType>(){}.getClass().getGenericSuperclass());
     }
 }
