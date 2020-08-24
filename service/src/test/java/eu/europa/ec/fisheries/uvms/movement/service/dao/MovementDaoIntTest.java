17f902e58f7aa22fee3ae9b556fd3ac83ba46c57
Johan Söderblom <johan.soderblom@havochvatten.se>
2020-08-24 17:19:11 +0200

    make the test slightly better...

--- service/src/test/java/eu/europa/ec/fisheries/uvms/movement/service/dao/MovementDaoIntTest.java
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/movement/service/dao/MovementDaoIntTest.java
@@ -564,13 +564,14 @@ public class MovementDaoIntTest extends TransactionalTests {
         
         assertThat(movements.size(), CoreMatchers.is(2));
         assertTrue(sortedIds.contains(movements.get(0).getId()));
+      //  assertThat(movements.get(0).getId(), CoreMatchers.is(sortedIds.get(0)));
         
         cursorPagination.setTimestampCursor(movements.get(1).getTimestamp());
         cursorPagination.setIdCursor(movements.get(1).getId());
         
         List<Movement> movements2 = movementDao.getCursorBasedList(cursorPagination);
-        
-        assertTrue(movements2.size() >=1);
+        assertTrue(movements2.size() == 1);
+        assertTrue(movements2.get(0).equals(movements.get(1)));
         assertTrue(sortedIds.contains(movements2.get(0).getId()));
     }
 
