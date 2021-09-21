4b87c0b05d82fb782635a656915da2497dd17e0c
johan <johan.soderblom@havochvatten.se>
2021-09-21 13:21:59 +0200

    cleanup

--- module/src/main/java/eu/europa/ec/fisheries/uvms/movement/rest/service/MovementRestResource.java
+++ module/src/main/java/eu/europa/ec/fisheries/uvms/movement/rest/service/MovementRestResource.java
@@ -218,28 +218,7 @@ public class MovementRestResource {
     }
 
     @POST
-    @Path("/track/assets/")
-    @RequiresFeature(UnionVMSFeature.viewMovements)
-    public Response getMovementTrackForAssets(@DefaultValue("") @QueryParam("startDate") String startDate, @DefaultValue("") @QueryParam("endDate") String endDate, TrackForAssetsQuery query) {
-        try {
-            if (query.getAssetIds().isEmpty()) {
-                return Response.ok(Collections.emptyList()).header("MDC", MDC.get("requestId")).build();
-            }
-
-            List<MovementSourceType> sourceTypes = RestUtilMapper.convertToMovementSourceTypes(query.getSources());
-            Instant startInstant = (endDate.isEmpty() ? Instant.now().minus(8, ChronoUnit.HOURS) : DateUtils.stringToDate(startDate));
-            Instant endInstant = (endDate.isEmpty() ? Instant.now() : DateUtils.stringToDate(endDate));
-            List<Movement> movements = movementDao.getMovementsForConnectIdsBetweenDates(query.getAssetIds(), startInstant, endInstant, sourceTypes);
-            List<MovementDto> movementDtos = MovementMapper.mapToMovementDtoList(movements);
-            return Response.ok(movementDtos).header("MDC", MDC.get("requestId")).build();
-        } catch (Exception e) {
-            LOG.error("Error when getting Micro Movement for connectIds: {}", query.getAssetIds(), e);
-            throw e;
-        }
-    }
-
-    @POST
-    @Path("/update")
+    @Path("/realtime")
     @RequiresFeature(UnionVMSFeature.viewMovements)
     public Response getLastMovementForAllAssets(List<String> sources) {
         try {
